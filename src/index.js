6f008973719cb48ec7a4a249691c9a0b510e123d
nipher <jonas@nipher.io>
2017-02-15 15:16:03 -0200

    Add table output report

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@
 import minimist from 'minimist'
 import getDependencies from './getDependencies'
 import getDiff from './getDiff'
-import createMessage from './createMessage'
+import parseDep from './parseDep'
+import Table from 'cli-table'
 
 const argv = minimist(process.argv.slice(2))
+const tbl = new Table({ head: ['Dependency', argv._[0], argv._[1], 'Difference'] })
 
 const deps = getDiff(
   getDependencies(argv._[0], argv.ignore),
   getDependencies(argv._[1], argv.ignore)
 )
 
-deps.map(createMessage).forEach(msg => console.log(msg))
+tbl.push(...deps.map(parseDep))
+console.log(tbl.toString())
--- test/parseDep.js
+++ test/parseDep.js
@@ -1,12 +1,12 @@
 import test from 'ava'
-import createMessage from '../src/createMessage'
+import parseDep from '../src/parseDep'
 import sinon from 'sinon'
 
 const chalkMock = {
   bold: {
-    green: sinon.stub().returns('-'),
-    red: sinon.stub().returns('-'),
-    white: sinon.stub().returns('-')
+    green: sinon.stub().returns('g'),
+    red: sinon.stub().returns('r'),
+    white: sinon.stub().returns('w')
   }
 }
 
@@ -22,25 +22,25 @@ const fakeDep = {
   right: '1.2.0'
 }
 
-test.before(t => createMessage.__Rewire__('chalk', chalkMock))
-test.after(t => createMessage.__ResetDependency__('chalk'))
+test.before(t => parseDep.__Rewire__('chalk', chalkMock))
+test.after(t => parseDep.__ResetDependency__('chalk'))
 
 test('returns messages for the dependency object', t => {
-  const message = createMessage(fakeDep)
+  const message = parseDep(fakeDep)
 
   t.is(chalkMock.bold.red.calledWith('1'), true)
   t.is(chalkMock.bold.green.calledWith('2'), true)
   t.is(chalkMock.bold.white.calledWith('1.'), true)
   t.is(chalkMock.bold.white.calledWith('.0'), true)
 
-  t.is(message, `${fakeDep.name}: diff = ---- | versions = 1.1.0 / 1.2.0`)
+  t.deepEqual(message, {[fakeDep.name]: ['1.1.0', '1.2.0', 'wrgw']})
 })
 
 test('uses N/D when appropiate', t => {
   const fakeDep2 = Object.assign({}, fakeDep)
   delete fakeDep2.left
   delete fakeDep2.right
-  const message = createMessage(fakeDep2)
+  const message = parseDep(fakeDep2)
 
-  t.is(message, `${fakeDep.name}: diff = ---- | versions = N/D / N/D`)
+  t.deepEqual(message, {[fakeDep.name]: ['N/D', 'N/D', 'wrgw']})
 })
